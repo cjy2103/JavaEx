16749daf7161f711f7224e69ced825d637042ea4
cjy2103 <43211800+cjy2103@users.noreply.github.com>
2019-10-08 23:39:09 +0900

    Update Test4.java

--- Extend/Test4.java
+++ Extend/Test4.java
@@ -1,6 +1,6 @@
 public class Test4 {
     public static void main(String[] args) {
-        Student student = new Student("123456789","±è¿µÈñ");
+        Student student = new Student("123456789","");
         System.out.println(student);
     }
 }
